f8eb9a7d2ab1f74ea76e326daf6cc0501e62b534
tonykold7 <34625199+tonykold7@users.noreply.github.com>
2017-12-17 17:32:39 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim; int x, y;
+             Console.WriteLine("====Ad girin====");
+             isim = Console.ReadLine();
+             x = isim.Length;
+             for (int i = 0; i < x; i++)
+             {
+                 y = (int)isim[i] + 10;
+                 Console.Write((char)y);
+             }
+             Console.ReadKey();
+            }
+        }
+    }
